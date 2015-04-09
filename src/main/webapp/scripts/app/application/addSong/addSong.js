367a6b8bbbfe5843580601bd1e64b385c6409dfc
unknown <champenois.natacha@gmail.com>
2015-04-09 17:06:54 +0200

    feat(gui): Create interface with text area
    TG-13 #closed

---
+++ src/main/webapp/scripts/app/application/addSong/addSong.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('socialjukeboxwebApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('addSong', {
+                parent: 'application',
+                url: '/addSong',
+                data: {
+                    roles: [],
+                    pageTitle : 'addSong.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/application/addSong/addSong.html',
+                        controller: 'addSongController'
+                    }
+                },
+                resolve: {
+                    mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                        $translatePartialLoader.addPart('addSong');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
