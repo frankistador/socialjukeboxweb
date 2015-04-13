845d808492e0f5472e5bfbe98396b3840ff15514
AUDREZ GUILLOT <guillot@MacBook-Air-de-GUILLOT.local>
2015-04-13 12:24:57 +0200

    no message

--- src/main/webapp/scripts/app/application/addSong/addSong.controller.js
+++ src/main/webapp/scripts/app/application/addSong/addSong.controller.js
@@ -1,9 +1,27 @@
 'use strict';
 
 angular.module('socialjukeboxwebApp')
-    .controller('addSongController', function ($scope, Principal) {
-        Principal.identity().then(function(account) {
-            $scope.account = account;
-            $scope.isAuthenticated = Principal.isAuthenticated;
-        });
+    .controller('addSongController', function ($scope, $http) {
+    	
+    
+    	$scope.postSong = function() {
+ 
+    		$scope.success=false;
+        	$scope.error=false;   		
+
+    		$http.post('/api/submission/submit?name='+$scope.title).
+    		  success(function(data, status, headers, config) {
+    			  $scope.success=true;
+    			  $scope.submittedSong=$scope.title;
+    			  $scope.title="";
+    		  }).
+    		  error(function(data, status, headers, config) {
+    			  $scope.error=true;
+    			  $scope.title=""; 
+    		  });
+    		
+    		
+    	}
+ 
+       
     });
