1ccb63470c5bedee03e2507e50b6a1e72d8cc956
wayglem <elias.salam@gmail.com>
2015-04-14 15:43:34 +0200

    feat(gui): creation of a playlit template which allows to see a list of song. TG-22 #closed

---
+++ src/main/webapp/scripts/app/application/userPlaylist/userPlaylist.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('socialjukeboxwebApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('userPlaylist', {
+                parent: 'application',
+                url: '/userPlaylist',
+                data: {
+                    roles: [],
+                    pageTitle : 'userPlaylist.title'
+
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/application/userPlaylist/userPlaylist.html',
+                        controller: 'userPlaylistController'
+                    }
+                },
+                resolve: {
+                    mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                        $translatePartialLoader.addPart('userPlaylist');
+                        return $translate.refresh();
+                    }]
+                }
+            });
+    });
