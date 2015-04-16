9c11523432f4443a2dec491bd32869780e24ce64
unknown <champenois.natacha@gmail.com>
2015-04-16 11:31:06 +0200

    feat(gui) : #13bis Create interface with text area (in user playlist web page)

--- src/main/webapp/scripts/app/application/playlists/userPlaylist.controller.js
+++ src/main/webapp/scripts/app/application/playlists/userPlaylist.controller.js
@@ -33,4 +33,24 @@ angular.module('socialjukeboxwebApp').
               })
 		};
 		getPlaylist();	
+		
+		$scope.postSong = function() {
+ 
+    		$scope.success=false;
+        	$scope.error=false;   		
+
+    		$http.post('/api/submission/submit?name='+$scope.title).
+    		  success(function(data, status, headers, config) {
+    			  $scope.success=true;
+    			  $scope.submittedSong=$scope.title;
+    			  $scope.title="";
+    		  }).
+    		  error(function(data, status, headers, config) {
+    			  $scope.error=true;
+    			  $scope.title=""; 
+    		  });
+    		
+    		
+    	}
+    	
     });
