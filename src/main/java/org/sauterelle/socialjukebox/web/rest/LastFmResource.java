0b5d5e86ba68ff94829658ebb0c7d256bba3a5db
wayglem <elias.salam@gmail.com>
2015-04-16 09:50:13 +0200

    feat(API): added search on youtube and on lastFM.
    TG-17 #closed

--- src/main/java/org/sauterelle/socialjukebox/web/rest/LastFmResource.java
+++ src/main/java/org/sauterelle/socialjukebox/web/rest/LastFmResource.java
@@ -1,20 +1,22 @@
 package org.sauterelle.socialjukebox.web.rest;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+import com.google.api.services.youtube.YouTube;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.sauterelle.socialjukebox.service.youtube.YoutubeSearch;
+import org.sauterelle.socialjukebox.web.rest.util.HTTPUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
@@ -24,44 +26,60 @@ private final String SERVICE_ENDPOINT= "http://www.last.fm/api/";
 private final String API_KEY = "523cb53942efb6cbac609f4e3a0c3a94";
 private final String SECRET = "is bde8cd4980b1633e91a6e1a46c6ee563";
 
-private final Logger log = LoggerFactory.getLogger(LastFmResource.class);
+    private static YouTube youtube;
 
+    private static final String PROPERTIES_FILENAME = "youtube.properties";
+    private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+
+
+    private final Logger log = LoggerFactory.getLogger(LastFmResource.class);
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView method() {
             return new ModelAndView("redirect:" + SERVICE_ENDPOINT + "auth/?api_key="+ API_KEY);
     }
-	
-	
-	
-	
-	@RequestMapping(value="/sign", method = RequestMethod.POST)
-	@ResponseBody
-	private byte[] sign(@RequestBody HashMap<String,String> params) throws NoSuchAlgorithmException{
-		log.debug("params = "+params);
-		return signMethod(params);
-	}
-	@RequestMapping(value="/test", method = RequestMethod.POST)
-	@ResponseBody
-	private byte[] test(@RequestBody String params) {
-		log.debug("params = "+params);
-		return params.getBytes();
-	}
+
+    @RequestMapping(value="/sign", method = RequestMethod.POST)
+    @ResponseBody
+    private byte[] sign(@RequestBody JSONObject params) throws NoSuchAlgorithmException, JSONException {
+        return signMethod(this.toParamsMap(params));
+    }
+
+    @RequestMapping(value="/search", method = RequestMethod.POST)
+    @ResponseBody
+    private String search(@RequestParam String songName) throws Exception {
+        String url = "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+songName+"&api_key="+API_KEY+"&format=json&limit=1";
+        url = url.replaceAll(" ","%20");
+        JSONObject response = HTTPUtils.getJson(url);
+        return response.getJSONObject("results").getJSONObject("trackmatches").getJSONObject("track").getString("url");
+    }
+
 	/*
 	 * TODO: Refactor this code.
 	 */
-	private byte[] signMethod(HashMap<String, String> parameters) throws NoSuchAlgorithmException{
+	private byte[] signMethod(Map<String, String> parameters) throws NoSuchAlgorithmException{
 		String toHash = "";
-		ArrayList<String> paramNames = (ArrayList<String>) parameters.keySet();
+		List<String> paramNames = new ArrayList<String>(parameters.keySet());
 		Collections.sort(paramNames);
 		for (String paramName : paramNames){
 			toHash += paramName + parameters.get(paramName);
 		}
+        toHash += SECRET;
 		byte[] hashMe = toHash.getBytes();
 		MessageDigest mg = MessageDigest.getInstance("md5");
 		hashMe = mg.digest(hashMe);
-		log.debug("hashed paramaters : "+hashMe);
 		return hashMe;
 	}
-	
+
+    public Map<String, String> toParamsMap(JSONObject json) throws JSONException {
+        Map<String, String> retMap = new HashMap<String, String>();
+        Iterator<String> keysItr = json.keys();
+        while(keysItr.hasNext()) {
+            String key = keysItr.next();
+            String value = (String) json.get(key);
+            retMap.put(key, value);
+        }
+        return retMap;
+    }
+
 }
