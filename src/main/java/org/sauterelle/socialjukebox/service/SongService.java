421391afa54a0194357c9132ef511fbf26281a29
Maud Bardin <maudbardin@MacBook-Pro-de-Maud.local>
2015-04-20 16:46:24 +0200

    feat(api) Add the song submitted to the current playlist in the database
    TG-42 #closed

--- src/main/java/org/sauterelle/socialjukebox/service/SongService.java
+++ src/main/java/org/sauterelle/socialjukebox/service/SongService.java
@@ -1,14 +1,17 @@
 package org.sauterelle.socialjukebox.service;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.inject.Inject;
 
 import org.sauterelle.socialjukebox.domain.Playlist;
 import org.sauterelle.socialjukebox.domain.Song;
+import org.sauterelle.socialjukebox.repository.PlaylistRepository;
 import org.sauterelle.socialjukebox.repository.SongRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,13 +23,23 @@ public class SongService {
 	   private final Logger log = LoggerFactory.getLogger(SongService.class);
 	   @Inject
 	   private SongRepository songRepository;
+	   @Inject
+	   private PlaylistRepository playlistRepository;
 	   private Song song;
+	   private Playlist playlist;
 	   
-	   public void saveSong(String name, String url){
+	   public void saveSong(String name, String url, Long idPlaylist){
+		   log.debug("name="+name+" url="+url +" id="+idPlaylist);
 		   song = new Song();
 		   song.setName(name);
 		   song.setUrl(url);
+		   playlist = playlistRepository.findOneWithEagerRelationships(idPlaylist);
+		   Set<Playlist> playlists= new HashSet<Playlist> ();
+		   playlist.getSongs().add(song);
+		   playlists.add(playlist);
+		   song.setPlaylists(playlists);
 		   songRepository.save(song);
+		   playlistRepository.save(playlist);
 	       }
 	   
 
