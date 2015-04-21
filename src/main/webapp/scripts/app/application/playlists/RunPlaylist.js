f686444481c8710500f3a24f5f0a4e76a51333cb
unknown <champenois.natacha@gmail.com>
2015-04-21 16:59:18 +0200

    feat(gui) : Create the html page to play a song TG-33 #closed feat(gui) : Include youtube iframe TG-34 #closed

---
+++ src/main/webapp/scripts/app/application/playlists/RunPlaylist.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('socialjukeboxwebApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('RunPlaylist', {
+                parent: 'application',
+                url: '/RunPlaylist',
+                data: {
+                    roles: ['ROLE_USER']
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/application/playlists/RunPlaylist.html',
+                        controller: 'RunPlaylistsController'
+                    }
+                },
+                resolve: {
+                
+                }
+            })
+    });
