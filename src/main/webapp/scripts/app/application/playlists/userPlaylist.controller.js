8ee342b964e97b152727aa1f0cf360c71c6f5631
wayglem <elias.salam@gmail.com>
2015-05-07 09:59:25 +0200

    fix: fixed share on facebook button. closes #77

--- src/main/webapp/scripts/app/application/playlists/userPlaylist.controller.js
+++ src/main/webapp/scripts/app/application/playlists/userPlaylist.controller.js
@@ -15,7 +15,7 @@ controller('userPlaylistController', function ($scope, Principal, ngTableParams,
     $scope.songs = [];
     $scope.urlParam=$stateParams.id;
     $scope.videoIterator = 0;
-    $scope.shareUrl = window.location.origin+window.location.pathname+"#/RunPlaylist/"+$scope.urlParam;
+    $scope.shareUrl = window.location.origin+window.location.pathname+"%23/RunPlaylist/"+$scope.urlParam;
     Principal.identity().then(function(account) {
         $scope.account = account;
         $scope.isAuthenticated = Principal.isAuthenticated;
